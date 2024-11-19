42362728815ded7f3eeb43f7bd2f5980f2c1fbfd
Fernando <fernandooogoncalves@gmail.com>
2024-11-19 11:27:44 -0300

    add: Menu de Opções no TKinter

--- tkinter_gui/index.py
+++ tkinter_gui/index.py
@@ -23,5 +23,20 @@ logo_label.place(x=40, y=20)
 title_label = Label(top_frame, text="ZOOLÓGICO ARCA DE NOÉ", font=("Century Gothic", 20), bg="black", fg="white")
 title_label.place(x=280, y=55)
 
+menu_label = Label(window, text="MENU DE OPÇÕES", font=("Century Gothic", 20, "bold"), width=20 , bg="darkorange", fg="black")
+menu_label.pack(pady=(60,0)) # utilizei somente o pack() para centralizar / padding Y, primeiro valor top, segundo valor bottom
+
+cad_button = Button(window, text="CADASTRAR", font=("Century Gothic", 15, "bold"), width=20 , bg="black", fg="darkorange", relief="raised")
+cad_button.pack(pady=5)
+alt_button = Button(window, text="ALTERAR", font=("Century Gothic", 15, "bold"), width=20 , bg="black", fg="darkorange", relief="raised")
+alt_button.pack(pady=5)
+ex_button = Button(window, text="EXCLUIR", font=("Century Gothic", 15, "bold"), width=20 , bg="black", fg="darkorange", relief="raised")
+ex_button.pack(pady=5)
+pes_button = Button(window, text="PESQUISAR", font=("Century Gothic", 15, "bold"), width=20 , bg="black", fg="darkorange", relief="raised")
+pes_button.pack(pady=5)
+
+bottom_frame = Frame(window, width=900, height=70, bg="black", relief="raised")
+bottom_frame.pack(side=BOTTOM)
+
 window.mainloop() # fim dos comandos para a janela
 
